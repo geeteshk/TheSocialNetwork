8780a1215366c14d12efa9f890d304201ef979f7
BudDavis <jimmie.davis@uta.edu>
2018-09-22 12:47:20 -0400

    Added some empty files to assist in setting up the project. Changed DDS IO to recv a vector of data.  Many other things !!

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+
+#include "ConsoleOutput.h"
+#include "DataStore.h"
+
+int main ( int argc, char* argv[] )
+{
+   std::cout << "TheSocialNetwork" << std::endl;
+   return 0;
+}
